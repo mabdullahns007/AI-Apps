bf421be1338a5217300030084c87e793d854e59b
Muhammad Abdullah <160305769+mabdullahns007@users.noreply.github.com>
2024-08-21 23:16:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import os
 
 # Set your Google API key
-os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
-
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCqMCBk2k1-pbACD3grHQIpiK7NKiDEx4A"
 
 st.title("YouTube Video Summarizer")
 st.write("Please enter a YouTube URL to get started.")
