39c41b1a5e8a281017a6ebffec0a70816bb5823a
Muhammad Abdullah <160305769+mabdullahns007@users.noreply.github.com>
2024-08-24 21:41:25 +0500

    Update app.py

--- PaperToExcel/app.py
+++ PaperToExcel/app.py
@@ -103,7 +103,7 @@ overall_chain = SequentialChain(
 # Streamlit app
 st.title("Paper To Excel")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpeg"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpeg","jpg","png"])
 
 def create_excel_file(data):
     # Convert string representation of list to actual list
